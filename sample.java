8276fd124bfe40b338ac98e47b6909a6429eafd7
sai <bejawadasaimahendra@gmail.com>
2024-08-01 09:31:51 +0530

    added at 1/8/24:9-31 am

---
+++ sample.java
@@ -0,0 +1,5 @@
+import java.util.*;
+public class sample {
+public static void main(String [] arg){
+System.out.println("Konichiwa watashi sai mahendra desuva andu this ish cloud devops desu  ");
+}}
